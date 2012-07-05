f74348b1ab2ee234947340fd5a4c62a39ada96e7
arielvb <i@arielvb.com>
2012-07-05 23:34:44 +0200

    Script per fer 'make' en windows

---
+++ ui/make.bat
@@ -0,0 +1,11 @@
+set UI_DIR=designer
+set BUILD_DIR=gen
+set PYQT_PATH=C:\Python27\Lib\site-packages\PyQt4
+echo # > %BUILD_DIR%\__init__.py
+call %PYQT_PATH%\pyuic4 -x %UI_DIR%\mainWindow.ui -o %BUILD_DIR%\mainWindow.py
+echo Next
+call %PYQT_PATH%\pyuic4 -x %UI_DIR%\dashboard.ui -o %BUILD_DIR%\dashboard.py
+call %PYQT_PATH%\pyuic4 -x %UI_DIR%\fitxa.ui -o %BUILD_DIR%\fitxa.py
+call %PYQT_PATH%\pyuic4 -x %UI_DIR%\search_results.ui -o %BUILD_DIR%\search_results.py
+call %PYQT_PATH%\pyuic4 -x %UI_DIR%\search_quick.ui -o %BUILD_DIR%\search_quick.py
+call %PYQT_PATH%\pyrcc4 -o %BUILD_DIR%\resources_rc.py %UI_DIR%\resources.qrc
