95d2abdd6573c54edd08e06ad165cda41cf6b3c9
arielvb <i@arielvb.com>
2012-07-10 01:33:07 +0200

    Fix wrong image path

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,15 +1,18 @@
+import os
+
+BASE_PATH = os.path.join(os.path.dirname(__file__), 'data')
 
 items = [
     {
         'title': 'The Pillars of the Earth',
         'designer': ['Michael Rieneck', 'Stefan Stadler'],
         'artist': ['Michael Menzel', 'Anke Pohl', 'Thilo Rick'],
-        'image': 'test/data/pilares.jpg'
+        'image': BASE_PATH + '/pilares.jpg'
     },
     {
         'title': 'Coney Island',
         'designer': ['Michael Schant'],
         'artist': ['Dennis Lohausen'],
-        'image': 'test/data/coney-island.jpg'
+        'image': BASE_PATH + '/coney-island.jpg'
     }
 ]
