1b9eb5fd9883c27a6647279f240b058bc2307185
arielvb <i@arielvb.com>
2012-07-16 20:15:19 +0200

    Test internet connection and add tearDown method

--- tests/application.py
+++ tests/application.py
@@ -5,12 +5,14 @@ from PyQt4.QtTest import QTest
 from PyQt4.QtCore import Qt
 
 
-
 class TestUi(unittest.TestCase):
 
     def setUp(self):
         '''Create the GUI'''
         self.app = QApplication(sys.argv)
+
+    def tearDown(self):
+        del self.app
 
     def test_application(self):
         from main import Ui_Application
@@ -19,7 +21,14 @@ class TestUi(unittest.TestCase):
         app.setupUi()
         self.assertIsInstance(app, Ui_MainWindow)
 
-
+    def test_network(self):
+        import urllib2
+        is_google = False
+        try:
+            urllib2.open('http://google.es', 2)
+            is_google = True
+        except:
+            self.assertTrue(is_google, "Looks that no intenet connection is avaible")
 
 if __name__ == '__main__':
     unittest.main()
