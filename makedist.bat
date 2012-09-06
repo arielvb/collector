5e15cec1af9acfa8927af1a534973012cae80ded
arielvb <i@arielvb.com>
2012-09-07 01:38:16 +0200

    Fixed: missing qt image plugins on windows dist

--- makedist.bat
+++ makedist.bat
@@ -1,2 +1,3 @@
 call make.bat
 C:\Python27\python.exe setup.py py2exe
+XCOPY "C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats" "C:\Documents and Settings\Ariel\Escritorio\collector\dist\imageformats" /I /S
