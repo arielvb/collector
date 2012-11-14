a66144852707f5c8ee6bf464492e36cef1dd4b32
arielvb <i@arielvb.com>
2012-11-14 23:39:13 +0100

    Update application testcase

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,35 +1,53 @@
 import unittest
-import sys
 import sip
 sip.setapi('QVariant', 2)
-from PyQt4.QtGui import QApplication
+from ui.application import CollectorApplication
 from PyQt4.QtTest import QTest
-from PyQt4.QtCore import Qt
+from PyQt4.QtCore import QCoreApplication
+import os
+import time
+import urllib2
+
+HOME = os.path.join(os.path.dirname(__file__), '..', 'data')
 
 
-class TestUi(unittest.TestCase):
+class TestApplication(unittest.TestCase):
 
     def setUp(self):
         '''Create the GUI'''
-        self.app = QApplication(sys.argv)
+        self.app = CollectorApplication(["--home", HOME], True)
+        # while QCoreApplication.startingUp():
+        #     time.sleep(2)
 
     def tearDown(self):
+        self.app.collector.shutdown()
+        self.app.quit()
+        CollectorApplication.current = None
+        CollectorApplication.collector = None
+        CollectorApplication.translators = {}
+        # while QCoreApplication.closingDown():
+        #     time.sleep(2)
         del self.app
 
-    def test_application_can_be_created(self):
+    def test_main_window(self):
         from ui.mainwindow import MainWindow
-        from ui.gen.mainWindow import Ui_MainWindow
-        app = MainWindow()
-        self.assertIsInstance(app, Ui_MainWindow)
+        assert isinstance(self.app.main, MainWindow)
 
-    # def test_network_is_active(self):
-    #     import urllib2
-    #     is_google = False
-    #     try:
-    #         urllib2.urlopen('http://google.com', timeout=200)
-    #         is_google = True
-    #     except:
-    #         self.assertTrue(is_google, "Looks like no internet connection is avaible.")
+    def test_discoverview(self):
+        action = self.app.main.actionDiscover
+        # QTest.mouseClick(action)
+        action.trigger()
+        # self.app.main.hide()
+        assert self.app.main.view == "discover"
+
+    def test_network_is_active(self):
+        is_google = False
+        try:
+            urllib2.urlopen('http://google.com', timeout=200)
+            is_google = True
+        except:
+            self.assertTrue(is_google, "Looks like no internet connection"
+                " is avaible.")
 
 if __name__ == '__main__':
     unittest.main()
